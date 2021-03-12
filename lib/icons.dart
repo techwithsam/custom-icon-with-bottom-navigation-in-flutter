e18dbd8f7256051d554fde1cc69199fa6dd15104
Samuel <samuelbeebest@gmail.com>
2021-03-12 07:33:56 +0100

    first commit

---
+++ lib/icons.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class MyFlutterApp {
+  MyFlutterApp._();
+
+  static const _kFontFam = 'MyFlutterApp';
+  static const String _kFontPkg = null;
+
+  static const IconData home = IconData(0xe800, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData saved = IconData(0xe801, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData download = IconData(0xe802, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+  static const IconData user = IconData(0xe803, fontFamily: _kFontFam, fontPackage: _kFontPkg);
+}
